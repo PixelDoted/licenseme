4a6202069e326944e8b41ad56b4283be96198283
PixelDots <isaac@marketplaceplus.com>
2023-08-30 16:47:17 -0500

    Fix cache path

--- src/util.rs
+++ src/util.rs
@@ -2,5 +2,5 @@ use std::path::PathBuf;
 
 pub fn get_cache() -> PathBuf {
     let home = home::home_dir().expect("No home directory.");
-    home.join(".cache/license")
+    home.join(".cache/licenseme")
 }
